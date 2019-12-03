33e7a2cc81e30d7615578476c21dd92dd9f0943a
Ubuntu <ubuntu@ip-172-31-40-218.eu-central-1.compute.internal>
2019-12-03 15:07:29 +0000

    pruebas para la api

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -69,6 +69,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:email, :password, :password_confirmation, :name)
+      params.require(:user).permit(:provider, :uid, :email, :first_name, :last_name, :picture, :created_at, :updated_at, :token)
     end
 end
