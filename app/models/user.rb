27b741f8b936ca60f53c884699a398bb87788910
Ubuntu <ubuntu@ip-172-31-5-95.us-east-2.compute.internal>
2019-12-03 12:32:36 +0000

    token no cambia

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,9 @@ class User < ApplicationRecord
 		where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
 			user.provider = auth.provider
 			user.uid = auth.uid
-			user.token = "asw_"+SecureRandom.urlsafe_base64
+			if user.token == nil then
+				user.token = "asw_"+SecureRandom.urlsafe_base64
+			end
 			user.first_name = auth.info.first_name
 			user.last_name = auth.info.last_name
 			user.email = auth.info.email
