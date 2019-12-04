421d635eec457b7fe0876f0c52bd4d7201e1c65d
Ubuntu <ubuntu@ip-172-31-22-2.us-east-2.compute.internal>
2019-12-04 15:55:26 +0000

    attachment

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -140,9 +140,10 @@ class IssuesController < ApplicationController
   end
   
   def show_attachment
-    @issue = Issue.find(params[:issue_id])
+    @issue = Issue.find(params[:id])
+    
       format.html {@file.name}
-      format.json {render json:{name: @issue.file}, status: :ok, each_serializer: IssueSerializer}
+      format.json {render json: @issue.id, status: :ok, each_serializer: IssueSerializer}
     
   end
   
