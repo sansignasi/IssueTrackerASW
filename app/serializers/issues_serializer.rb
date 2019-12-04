4340e095e018f5f388306b89662cd3a81fb8ba08
Ubuntu <ubuntu@ip-172-31-22-2.us-east-2.compute.internal>
2019-12-04 15:14:18 +0000

    attachment

--- app/serializers/issues_serializer.rb
+++ app/serializers/issues_serializer.rb
@@ -1,6 +1,9 @@
 class IssuesSerializer < ActiveModel::Serializer
+     include Rails.application.routes.url_helpers
+
   attributes :id, :Title, :Description, :Type, :Priority, :Status,:Asigned,:Creator,:Created,:Updated,:file
+   
    def file
-    rails_blob_path(object.file, only_path: true) if object.file.attached?
+        rails_blob_path(object.file, only_path: true) if object.file.attached?
    end
 end
