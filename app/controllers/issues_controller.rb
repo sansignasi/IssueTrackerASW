5d2d55b254fb5f964028e45b6ee69bd537fec591
Ubuntu <ubuntu@ip-172-31-40-218.eu-central-1.compute.internal>
2019-12-04 12:12:58 +0000

    delete de una issue

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -79,7 +79,7 @@ class IssuesController < ApplicationController
   def destroy
     @issue.destroy
     respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+      format.html
       format.json { head :no_content }
     end
   end
